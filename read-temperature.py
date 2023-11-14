ca209fea4124eec0ab9c6a4cb81e892bbd2066af
dmatherpi <55391764+dmatherpi@users.noreply.github.com>
2023-11-14 22:20:21 +0000

    Create read-temperature.py
    Initial commit

---
+++ read-temperature.py
@@ -0,0 +1,40 @@
+#	Temperature Sensor Data Viewer
+#	Author: dmatherpi
+
+#-----Notes
+# Code written in Python to run on Raspberry Pi's with a temperature sensor connected via GPIO
+# Replace line 17 address with the address to your local temperature sensor
+
+#-----Initialise the System and load the GPIO modprobe
+
+import os
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+#-*- coding: utf-8 -*-
+
+#------Define the temperature sensor
+
+temp_sensor1 = '/sys/bus/w1/devices/28-0414684b00ff/w1_slave'
+
+#------Read Temperature Sensor 1 (temp_sensor1)
+
+def temp_raw1():
+    f = open(temp_sensor1, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp1():
+    lines = temp_raw1()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = temp_raw1()
+    temp_output1 = lines[1].find('t=')
+    if temp_output1 != -1:
+        temp_string1 = lines[1][temp_output1+2:]
+        temp_c1 = float(temp_string1) / 1000
+        return temp_c1
+	
+#-----Output - the current temperature in celsius
+
+print (str(read_temp1()))
